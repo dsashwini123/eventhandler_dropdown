acf5f5c18fba876b39f31f283da8712307ee53f6
Ashwini DS <ashwini.ds@appliedsystems.com>
2024-08-23 15:42:20 +0530

    drop-down

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 document.getElementById('lob').addEventListener('change', function() {
+    // this element points to lob selected or changed.
     const element = this;
+    // value will have the lob selected.
     const selectedLob = element.value;
     document.getElementById('selected-lob').textContent = `You selected: ${selectedLob}`;
 });
